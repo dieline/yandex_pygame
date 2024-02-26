242a29bb8102362a646c1ddda7f47e2552591a77
dieline <rainbowskyven@gmail.com>
2024-02-26 18:53:14 +1000

    Collisions 1.0

--- map.py
+++ map.py
@@ -1,10 +1,11 @@
 from settings import *
+import pygame
 
 text_map = [
     'wwwwwwwwwwww',
     'w..........w',
-    'w....w.....w',
-    'w....w.....w',
+    'w...w......w',
+    'w...w......w',
     'w...w......w',
     'w.......wwww',
     'wwww.......w',
@@ -12,8 +13,10 @@ text_map = [
 ]
 world_map = set()
 mini_map = set()
+collision_walls = []
 for j, row in enumerate(text_map):
     for i, char in enumerate(row):
         if char == 'w':
             world_map.add((i * TILE, j * TILE))
             mini_map.add((i * MAP_TILE, j * MAP_TILE))
+            collision_walls.append(pygame.Rect(i * TILE, j * TILE, TILE, TILE))
